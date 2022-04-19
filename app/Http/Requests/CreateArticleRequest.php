78c15e905ab1279f19f68322df66876e373c4626
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2022-04-19 12:38:53 +0100

    fix refactor

--- app/Http/Requests/CreateArticleRequest.php
+++ app/Http/Requests/CreateArticleRequest.php
@@ -2,22 +2,10 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class CreateArticleRequest extends FormRequest
+class CreateArticleRequest extends ValidatorRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
+     * Returns the validation rules applied in the request
      *
      * @return array
      */
