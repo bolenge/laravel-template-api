ee485af92bafa0b978f2a91e148e43ed25fdc31d
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2022-04-19 12:18:33 +0100

    fix store request params

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,17 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateArticleRequest;
 use App\Models\Article;
-use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function store(Request $request)
+    public function store(CreateArticleRequest $request)
     {
-        $articleCreated = Article::create([
-            'title' => $request->title,
-            'body' => $request->body,
-        ]);
+        $articleCreated = Article::create($request->all());
 
         return response()->json($articleCreated, 201);
     }
